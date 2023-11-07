8c0ec7617f41b10345b83d675e92e698cb03a764
Scott <scott.axios@gmail.com>
2023-11-07 13:54:25 -0800

    Update remove auth block.ps1

--- remove auth block.ps1
+++ remove auth block.ps1
@@ -6,19 +6,20 @@ if (Test-Path $webConfigPath) {
     # Load the web.config as an XML document
     $webConfig = [xml](Get-Content $webConfigPath)
 
-    # Find the <authentication> element under <system.webServer>
-    $authNode = $webConfig.configuration.'system.webServer'.authentication
+    # Find the <authentication> element under <system.webServer><security>
+    $securityNode = $webConfig.configuration.'system.webServer'.security
+    $authNode = $securityNode.authentication
 
     # If the <authentication> node exists, remove it
     if ($authNode -ne $null) {
-        $webConfig.configuration.'system.webServer'.RemoveChild($authNode)
+        $securityNode.RemoveChild($authNode)
 
         # Save the updated web.config
         $webConfig.Save($webConfigPath)
         Write-Output "The <authentication> block has been removed from the web.config file."
     }
     else {
-        Write-Output "No <authentication> block found in the <system.webServer> section of the web.config file."
+        Write-Output "No <authentication> block found in the <security> section of the <system.webServer> in the web.config file."
     }
 }
 else {
